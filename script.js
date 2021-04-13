16f803ee01825e40a99e9d183d747fbf45af8e60
tatkuptcova <tatkuptcova@yandex.ru>
2021-04-13 17:07:54 +0200

    добавлены стили popup

---
+++ script.js
@@ -0,0 +1,8 @@
+/*function renderLoading(isLoading, button) {
+    if (isLoading) {
+      button.textContent = "Сохранение..";
+    } else {
+      button.textContent = "Сохранить";
+    }
+  }
+   
