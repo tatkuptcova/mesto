2605dcc9f3d40663e10a222d08af949249700482
tatkuptcova <tatkuptcova@yandex.ru>
2021-04-14 22:35:35 +0200

    push open/close

--- script/index.js
+++ script/index.js
@@ -14,24 +14,6 @@ editButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
   
 
-/*const popupSave = document.querySelector('.popup__button-submit');
-const popupName = document.querySelector('.popup__item_input-name');
-const popupAbout = document.querySelector('.popup__item_input-about');
-
-const nameInput = document.querySelector('.popup__item_input-name');
-const nameProfile = document.querySelector('.profile__name');
-
-const profileAbout = document.querySelector('.profile__about');
-const descriptionInput = document.querySelector('.popup__item_input-about');
-
-const popupButtonAbout = document.querySelector('.popup__button-submit-edit');
-
-function saveItem(){
-    popupSave.className.remove('popup__button-submit-edit');
-}
-
-popupButtonAbout.addEventListener('click', saveItem);*/
-
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__button-submit');
@@ -45,11 +27,7 @@ function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
-    const nameInput = document.querySelector(".popup__item_input-name");
-    const nameProfile = (".profile__name");
     // Получите значение полей jobInput и nameInput из свойства value
-    const jobInput = document.querySelector('.popup__item_input-about');
-    const profileAbout = (".profile__about");
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
