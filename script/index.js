6a58e9dc11da46367bcf3407ef9a309697130656
tatkuptcova <tatkuptcova@yandex.ru>
2021-04-15 22:01:54 +0200

    активирована кнопка Сохранить

--- script/index.js
+++ script/index.js
@@ -16,10 +16,14 @@ popupClose.addEventListener('click', closePopup);
 
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__button-submit');
+let formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__item_input-name');
 let jobInput = document.querySelector('.popup__item_input-about');
+
+let nameDisplay = document.querySelector('#profileName');
+let jobDisplay = document.querySelector('#profileAbout');
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -29,13 +33,16 @@ function formSubmitHandler(evt) {
     // О том, как это делать, расскажем позже.
     // Получите значение полей jobInput и nameInput из свойства value
     // Выберите элементы, куда должны быть вставлены значения полей
-
+    nameDisplay.textContent = nameInput.value
+    jobDisplay.textContent = jobInput.value
     // Вставьте новые значения с помощью textContent
+    closePopup()
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
 
 let likeButtons = document.querySelectorAll('.button__like');
 // for (let button of likeButtons) {
