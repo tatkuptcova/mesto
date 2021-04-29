b86b51b2b7a28873f9c50d001b13d52f7d6ea042
tatkuptcova <tatkuptcova@yandex.ru>
2021-04-29 17:26:00 +0200

    подключен popup_card в js

--- script/index.js
+++ script/index.js
@@ -1,58 +1,56 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popupClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const popupClose = document.querySelector('.popup__close');
+const popupProfile = document.querySelector('.popup_profile');
 
+const addButton = document.querySelector('.profile__button-add');
+const popupNewCard = document.querySelector('.popup_card');
+const profileButtonAdd = document.querySelector('.profile__button-add');
 
-let formElement = document.querySelector('.popup__form');
+let formElementProfile = document.querySelector('.popup__form_profile');
 
 let nameInput = document.querySelector('.popup__item_input-name');
 let jobInput = document.querySelector('.popup__item_input-about');
 
-let titleInput = document.querySelector('.popup__item_input-title');
-let linkInput = document.querySelector('.popup__item_input-link');
-
 let nameDisplay = document.querySelector('#profileName');
 let jobDisplay = document.querySelector('#profileAbout');
 
-let titleDisplay = document.querySelector('#placesTitle');
-let linkDisplay = document.querySelector('#placesLink');
+let formElementCard = document.querySelector('.popup__form_add');
+let titelDisplay = document.querySelector('#nameplace');
+let linkDisplay = document.querySelector('#place');
+let titleInput = document.querySelector('.popup__item_input-title');
+let linkInput = document.querySelector('.popup__item_input-link');
 
 let likeButtons = document.querySelectorAll('.places__button-like');
 
-
-
-function openPopup() { 
-    popup.classList.add('popup_opened');
-    nameInput.value = nameDisplay.textContent;
-    jobInput.value = jobDisplay.textContent;
-    titleInput.value = titleDisplay.textContent;
-    linkInput.value = linkDisplay.textContent;
+function openPopupProfile() { 
+    popupProfile.classList.add('popup_opened');
 }
-
-//function openPopup() {
-  //  popup.classList.add('popup_opened');
-  //  titleInput.value = titleDisplay.textContent;
-  //  linkInput.value = linkDisplay.textContent;
-//}
   
-function closePopup() { 
-    popup.classList.remove('popup_opened');
+function closePopupProfile() { 
+    popupProfile.classList.remove('popup_opened');
+}
+  
+function openPopupCards(){
+    popupNewCard.classList.toggle('popup_opened');
 }
 
+function closePopupCards(){
+    popupNewCard.classList.remove('popup_opened');
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
     nameDisplay.textContent = nameInput.value
     jobDisplay.textContent = jobInput.value
-    closePopup()
+    closePopupProfile()
 }
 
-//function formSubmitHandler(evt) {
-  //  evt.preventDefault();
-    //titleDisplay.textContent = titleInput.value
-    //linkDisplay.textContent = linkInput.value
-//}
-
+function formSubmitHandler(evt) {
+    evt.preventDefault(); 
+    titelDisplay.textContent = titleInput.value
+    linkDisplay.textContent = linkInput.value
+    closePopupCards()
+}
 
 likeButtons.forEach(function(button) {
     button.addEventListener(
@@ -62,18 +60,21 @@ likeButtons.forEach(function(button) {
 })
 
 
+editButton.addEventListener('click', openPopupProfile);
+popupClose.addEventListener('click', closePopupProfile);
 
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+addButton.addEventListener('click', openPopupCards);
+popupNewCard.addEventListener('click', closePopupCards);
 
 
-formElement.addEventListener('submit', formSubmitHandler);
-
+formElementProfile.addEventListener('submit', formSubmitHandler);
+formElementCard.addEventListener('submit', formSubmitHandler);
 
 
 // for (let button of likeButtons) {
 //     button.addEventListener(
 //         'click', () => 
-//             button.classList.toggle('places__button-like_active')
+//             button.classList.toggle('place__button-like_active')
 //     )    
-//}
+// }
+
