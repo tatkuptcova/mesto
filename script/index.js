345d776dcd0af0d0dd1755520a81224f85dd9897
tatkuptcova <tatkuptcova@yandex.ru>
2021-04-29 12:09:56 +0200

    popup_card

--- script/index.js
+++ script/index.js
@@ -8,14 +8,14 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__item_input-name');
 let jobInput = document.querySelector('.popup__item_input-about');
 
-//let titleInput = document.querySelector('.popup__item_input-title');
-//let linkInput = document.querySelector('.popup__item_input-link');
+let titleInput = document.querySelector('.popup__item_input-title');
+let linkInput = document.querySelector('.popup__item_input-link');
 
 let nameDisplay = document.querySelector('#profileName');
 let jobDisplay = document.querySelector('#profileAbout');
 
-//let titleDisplay = document.querySelector('#placesTitle');
-//let linkDisplay = document.querySelector('#placesLink');
+let titleDisplay = document.querySelector('#placesTitle');
+let linkDisplay = document.querySelector('#placesLink');
 
 let likeButtons = document.querySelectorAll('.places__button-like');
 
@@ -25,6 +25,8 @@ function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = nameDisplay.textContent;
     jobInput.value = jobDisplay.textContent;
+    titleInput.value = titleDisplay.textContent;
+    linkInput.value = linkDisplay.textContent;
 }
 
 //function openPopup() {
@@ -74,36 +76,4 @@ formElement.addEventListener('submit', formSubmitHandler);
 //         'click', () => 
 //             button.classList.toggle('places__button-like_active')
 //     )    
-// }
-
-
-
-
-//class Popup {
- //   constructor() {
-   //     this.add = document.querySelector('#popupAdd');
-   //     this.submitAddButton = this.add.querySelector('.popup__button');
-   //     this.edit = document.querySelector('#popupEdit');
-   //     this.submitEditButton = this.edit.querySelector('.popup__button');
-   //     this.pic = document.querySelector('#popupPic');
-    //}
-   // open(event) {
-   //     if (event.target.classList.contains('user-info__button')) {
-   //         this.add.classList.add('popup_is-opened');
-   //     }
-   //     if (event.target.classList.contains('user-info__edit-button')) {
-   //         this.edit.classList.add('popup_is-opened');
-   //         fullName.value = profileName.textContent;
-   //         bio.value = profileJob.textContent;
-   //         openingValidation(editForm);
-   //     }
-    //    if (event.target.classList.contains('place-card__image')) {
-    //        this.pic.classList.add('popup_is-opened');
-    //        this.pic.querySelector('.popup__picture').setAttribute('src', event.target.getAttribute('style').slice(22, -1));
-    //    }
-    //}
-    //close(event) {
-     //   event.path[2].classList.remove('popup_is-opened');
-
- // }
 //}
