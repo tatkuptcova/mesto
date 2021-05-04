457e48e0bfc372c2836f5f01d8c1f2a77a3405cf
tatkuptcova <tatkuptcova@yandex.ru>
2021-05-04 07:33:32 +0200

    добавлена папка initial-ccards.js, исправлен innerHTML на textContent

--- script/index.js
+++ script/index.js
@@ -65,70 +65,7 @@ popupCloseAdd.addEventListener('click', closePopupNewCard);
 
 formElementNewCard.addEventListener('submit', formSubmitNewCardHandler);
 
-const elementsList = document.querySelector('.elements__catalogue');
-const elementTemplate = document.querySelector('#cards-template');
 
-
-const initialCards = [
-  {
-    name: 'Монт-Сан Мишель',
-    link: 'https://images.unsplash.com/photo-1596436831831-87dd84a69101?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Мичиган',
-    link: 'https://images.unsplash.com/photo-1618767438211-11ffdb8e1861?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-
-function addCard(title, link) {
-  const cardElement = elementTemplate.content.querySelector('.elements__item').cloneNode(true);
-
-  const caption = cardElement.querySelector('.elements__title');
-  caption.innerHTML = title
-
-  const img = cardElement.querySelector('.elements__image');
-  
-  img.src = link;
-  img.addEventListener(
-    'click', () => 
-      openPopupPic(img.src, caption.innerHTML)
-  )
-
-  const likeButton = cardElement.querySelector('.elements__button-like');
-  likeButton.addEventListener(
-    'click', () => 
-      likeButton.classList.toggle('elements__button-like_active')
-  );
-
-  const deleteButton = cardElement.querySelector('.elements__button-delete');
-  deleteButton.addEventListener(
-    'click', () =>
-    cardElement.remove()
-  )
-  
-  elementsList.prepend(cardElement);
-}
-
-initialCards.forEach((element) => {
-  addCard(element.name, element.link);
-})
 
 const popupPic = document.querySelector('.popup_pic');
 const popupCloseImage = document.querySelector('.popup__close_image');
@@ -144,7 +81,7 @@ function openPopupPic(link, title) {
   popupImage = document.querySelector('.popup__image');
   popupImage.src = link
   popupCaption = document.querySelector('.popup__caption');
-  popupCaption.innerHTML = title
+  popupCaption.textContent = title
 }
 
 function closePopupPic() { 
---
+++ script/initial-cards.js
@@ -0,0 +1,64 @@
+const elementsList = document.querySelector('.elements__catalogue');
+const elementTemplate = document.querySelector('#cards-template');
+
+
+const initialCards = [
+  {
+    name: 'Монт-Сан Мишель',
+    link: 'https://images.unsplash.com/photo-1596436831831-87dd84a69101?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Мичиган',
+    link: 'https://images.unsplash.com/photo-1618767438211-11ffdb8e1861?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+function addCard(title, link) {
+  const cardElement = elementTemplate.content.querySelector('.elements__item').cloneNode(true);
+
+  const caption = cardElement.querySelector('.elements__title');
+  caption.textContent = title
+
+  const img = cardElement.querySelector('.elements__image');
+  
+  img.src = link;
+  img.addEventListener(
+    'click', () => 
+      openPopupPic(img.src, caption.textContent)
+  )
+
+  const likeButton = cardElement.querySelector('.elements__button-like');
+  likeButton.addEventListener(
+    'click', () => 
+      likeButton.classList.toggle('elements__button-like_active')
+  );
+
+  const deleteButton = cardElement.querySelector('.elements__button-delete');
+  deleteButton.addEventListener(
+    'click', () =>
+    cardElement.remove()
+  )
+  
+  elementsList.prepend(cardElement);
+}
+
+initialCards.forEach((element) => {
+  addCard(element.name, element.link);
+})
