b1af7c263e94b710a528427bfdd63b59ed8fc1ba
tatkuptcova <tatkuptcova@yandex.ru>
2021-05-19 19:49:00 +0200

    переименован overlayClick = popupList, event = popup

--- script/index.js
+++ script/index.js
@@ -67,16 +67,14 @@ const validationConfig = {
     errorActiveClass: 'popup__input-error_active',
 };
 
-const overlayClick = document.querySelectorAll('.popup');
-overlayClick.forEach((event) => {
-    event.addEventListener( 'click', (event) => {
-        if (event.target === event.currentTarget) {
-            event.target.closest('.popup').classList.remove('popup_opened');
+const popupList= document.querySelectorAll('.popup');
+popupList.forEach((popup) => {
+    popup.addEventListener( 'click', (popup) => {
+        if (popup.target === popup.currentTarget) {
+            popup.target.closest('.popup').classList.remove('popup_opened');
         };
     });
 });
-
-
 
 function addCard(cardElement) { 
     elementsList.prepend(cardElement);
@@ -96,7 +94,6 @@ function formSubmitNewCardHandler(evt) {
     closePopup(popupNewCard)
 }
 
-
 function openPopupPic(link, title) {
     // popupImage = document.querySelector('.popup__image');
     popupImage.src = link
@@ -105,7 +102,6 @@ function openPopupPic(link, title) {
     popupCaption.alt = title
     openPopup(popupPic)
 }
-
 
 function openPopup(popup) {
     popup.classList.add('popup_opened');
