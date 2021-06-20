5821b03549a19c97b555a196b746e63d0b49fe0b
tatkuptcova <tatkuptcova@yandex.ru>
2021-06-20 17:20:27 +0200

    this._popupImage = document.querySelector(.popup__image)=> this._popupImage = this._popupSelector.querySelector(.popup__image);  open()=>open(title,link)

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,15 +3,14 @@ import Popup from '../components/Popup.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector, title, link) {
         super(popupSelector);
-        this._popupImage = document.querySelector('.popup__image');
-        this._popupCaption = document.querySelector('.popup__caption');
-        this._title = title
-        this._link = link
+        this._popupImage = this._popupSelector.querySelector('.popup__image');
+        this._popupCaption = this._popupSelector.querySelector('.popup__caption');
     }
 
-    open(){
-        this._popupImage.src = this._link;
-        this._popupCaption.textContent = this._title;
+    open(title, link){
+        this._popupImage.src = link;
+        this._popupImage.alt = title;
+        this._popupCaption.textContent = title;
         super.open();
     }
 }
